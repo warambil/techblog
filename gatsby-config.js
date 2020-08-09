004b4d8a7ab76d426a72d7bc1851fca4fcb98977
Wilman Arambillete <warambil@gmail.com>
2020-08-09 19:59:24 -0300

    google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,17 @@ module.exports = {
     title: `w Blog`,
     description: `Sharing good stuff`,
     author: `Wilman Arambillete`,
-    siteUrl: `http://warambil.com`
+    siteUrl: `http://warambil.com`,
   },
   plugins: [
     `gatsby-plugin-dark-mode`,
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-21833652-1",
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
@@ -62,7 +68,7 @@ module.exports = {
         ],
       },
     },
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -80,26 +86,29 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [{
-          resolve: `gatsby-remark-vscode`,
-          // All options are optional. Defaults shown here.
-          options: {
-            theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
-            wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
-            injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
-            extensions: [],         // Third-party extensions providing additional themes and languages
-            languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
-            replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-            getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
-              content,              //   - the string content of the line
-              index,                //   - the zero-based index of the line within the code fence
-              language,             //   - the language specified for the code fence
-              meta                  //   - any options set on the code fence alongside the language (more on this later)
-            }) => '',
-            logLevel: 'warn'       // Set to 'info' to debug if something looks wrong
-          }
-        }]
-      }
+        plugins: [
+          {
+            resolve: `gatsby-remark-vscode`,
+            // All options are optional. Defaults shown here.
+            options: {
+              theme: "Dark+ (default dark)", // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: "", // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [], // Third-party extensions providing additional themes and languages
+              languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({
+                // Function allowing dynamic setting of additional class names on individual lines
+                content, //   - the string content of the line
+                index, //   - the zero-based index of the line within the code fence
+                language, //   - the language specified for the code fence
+                meta, //   - any options set on the code fence alongside the language (more on this later)
+              }) => "",
+              logLevel: "warn", // Set to 'info' to debug if something looks wrong
+            },
+          },
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
